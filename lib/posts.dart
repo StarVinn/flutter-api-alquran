b4fab101b5463cddc7d56ba63a37bb72532d64c2
dementriadelvin <dementriadelvin22@gmail.com>
2025-02-10 12:02:40 +0700

    add searchbar on posts

--- lib/posts.dart
+++ lib/posts.dart
@@ -3,106 +3,145 @@ import 'http_service.dart';
 import 'post_model.dart';
 import 'post_details.dart';
 
-class PostsPage extends StatelessWidget {
-  final HttpService httpService = HttpService();
+class PostsPage extends StatefulWidget {
+  @override
+  _PostsPageState createState() => _PostsPageState();
+}
 
-  PostsPage({super.key});
+class _PostsPageState extends State<PostsPage> {
+  final HttpService httpService = HttpService();
+  List<Surah> surahList = [];
+  List<Surah> filteredSurahList = [];
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchSurah();
+    searchController.addListener(() {
+      filterSurah();
+    });
+  }
+
+  void fetchSurah() async {
+    surahList = await httpService.getSurah();
+    setState(() {
+      filteredSurahList = surahList;
+    });
+  }
+
+  void filterSurah() {
+    String query = searchController.text.toLowerCase();
+    setState(() {
+      filteredSurahList = surahList.where((surah) {
+        return surah.nama_latin.toLowerCase().contains(query);
+      }).toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Al-Quran',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Al-Quran',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              width: 200, // Adjust width as needed
+              child: TextField(
+                controller: searchController,
+                decoration: InputDecoration(
+                  hintText: 'Cari Surah...',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
         backgroundColor: Colors.teal,
       ),
-      body: FutureBuilder<List<Surah>>(
-        future: httpService.getSurah(),
-        builder: (context, AsyncSnapshot<List<Surah>> snapshot) {
-          if (snapshot.hasData) {
-            final surahList = snapshot.data!;
-            return ListView.builder(
-              itemCount: surahList.length,
-              itemBuilder: (context, index) {
-                final surah = surahList[index];
-                return Card(
-                  elevation: 3,
-                  margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
+      body: ListView.builder(
+        itemCount: filteredSurahList.length,
+        itemBuilder: (context, index) {
+          final surah = filteredSurahList[index];
+          return Card(
+            elevation: 3,
+            margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.teal,
+                child: Text(
+                  surah.nomor.toString(),
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.teal,
-                      child: Text(
-                        surah.nomor.toString(),
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      surah.nama_latin,
+                ),
+              ),
+              title: Text(
+                surah.nama_latin,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black87,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Jumlah Ayat: ${surah.jumlah_ayat}",
                       style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.black87,
+                        fontSize: 14,
+                        color: Colors.blueGrey,
                       ),
                     ),
-                    subtitle: Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Jumlah Ayat: ${surah.jumlah_ayat}",
-                            style: const TextStyle(
-                              fontSize: 14,
-                              color: Colors.blueGrey,
-                            ),
-                          ),
-                          Text(
-                            "Tempat Turun: ${surah.tempat_turun}",
-                            style: const TextStyle(
-                              fontSize: 14,
-                              color: Colors.blueGrey,
-                            ),
-                          ),
-                          Text(
-                            "Arti: ${surah.arti}",
-                            style: const TextStyle(
-                              fontSize: 14,
-                              color: Colors.blueGrey,
-                            ),
-                          ),
-                        ],
+                    Text(
+                      "Tempat Turun: ${surah.tempat_turun}",
+                      style: const TextStyle(
+                        fontSize: 14,
+                        color: Colors.blueGrey,
                       ),
                     ),
-                    onTap: () async {
-                      // Fetch detailed Surah data when tapped
-                      final surahDetail = await httpService.getSurahDetail(surah.nomor);
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SurahDetailPage(
-                            surahDetail: surahDetail,
-                          ),
-                        ),
-                      );
-                    },
+                    Text(
+                      "Arti: ${surah.arti}",
+                      style: const TextStyle(
+                        fontSize: 14,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              onTap: () async {
+                final surahDetail =
+                    await httpService.getSurahDetail(surah.nomor);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SurahDetailPage(
+                      surahDetail: surahDetail,
+                    ),
                   ),
                 );
               },
-            );
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
+            ),
+          );
         },
       ),
     );
